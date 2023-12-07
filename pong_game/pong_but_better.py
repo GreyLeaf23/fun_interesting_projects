3a9d89bcbf5f6f948d4f4e41b0a2996197303daa
GreyLeaf23 <christian.torres2894@gmail.com>
2023-12-07 13:37:55 -0400

    First checkpoint! (12:20 min.?)

--- pong_game/pong_but_better.py
+++ pong_game/pong_but_better.py
@@ -10,7 +10,7 @@ wn = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong_But_Better")
 
 
-# Variable that allows control  program RUN
+# Variable that allows control program RUN
 run = True
 
 # Colors in RGB
@@ -20,6 +20,7 @@ RED = (255, 0, 0)
 # Ball creation
 radius = 15
 ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
+ball_vel_x, ball_vel_y = 0.7, 0.7
 
 # Paddle Creation
 paddle_width, paddle_height = 20, 120
@@ -30,9 +31,16 @@ left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH - (100 - paddle_widt
 # Spawn ball in screen
 pygame.draw.circle(wn, BLUE, (ball_x, ball_y), radius)
 
+# Ball movements
+ball_x += ball_vel_x
+ball_y += ball_vel_y
+
+
 # Spawn paddles in screen
 pygame.draw.rect(wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
+
 pygame.draw.rect(wn, RED, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
+
 
 # Update game to current state
 pygame.display.update()
